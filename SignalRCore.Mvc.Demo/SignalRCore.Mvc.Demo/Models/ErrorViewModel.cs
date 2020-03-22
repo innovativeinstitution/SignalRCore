f28e887cf448cf7b617d4ffa113b80ca96409c07
innovativeinstitution <innovativeinstitution@gmail.com>
2020-03-22 15:31:01 -0700

    Initial Commit for SignalR Core

---
+++ SignalRCore.Mvc.Demo/SignalRCore.Mvc.Demo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SignalRCore.Mvc.Demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
